493a20af9fdaaa48a6fe0778ee265eb68d8ae7a6
Soumodip Das <soumodipd567@gmail.com>
2022-12-05 15:12:11 +0530

    comt

--- php&mySQL/chapter2.1/index.php
+++ php&mySQL/chapter2.1/index.php
@@ -1,5 +1,16 @@
 <?php
 
+//connect to database
+$conn = mysqli_connect('localhost', 'pekka', 'pekka1234', "pekka's_play_house");
+
+//check connection
+if(!$conn){
+  echo 'Connection error:' .mysqli_connect_error();
+}
+
+//write query for all houses
+$sql = 'SELECT title, components, id FROM houses'
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
