60659b893693f2823819190e8b8245474556b36c
Soumodip Das <soumodipd567@gmail.com>
2022-12-12 11:12:32 +0530

    DNE

--- php&mySQL/chapter2.2/sandbox.php
+++ php&mySQL/chapter2.2/sandbox.php
@@ -4,10 +4,20 @@
 
 //$_GET['name'], $_POST['name']
 
-echo $_SERVER['SERVER_NAME'] . '<br/>';
-echo $_SERVER['REQUEST_METHOD'] . '</br>';
-echo $_SERVER['SCRIPT_FILENAME'] . '</br>';
-echo $_SERVER['PHP_SELF'] . '</br>';
+//echo $_SERVER['SERVER_NAME'] . '<br/>';
+//echo $_SERVER['REQUEST_METHOD'] . '</br>';
+//echo $_SERVER['SCRIPT_FILENAME'] . '</br>';
+//echo $_SERVER['PHP_SELF'] . '</br>';
+
+//session
+if(isset($_POST['submit'])){
+    session_start();
+
+    $_SESSION['name'] = $_POST['name'];
+
+    echo $_SESSION['name'];
+}
+
 
 ?>
 
@@ -17,6 +27,11 @@ echo $_SERVER['PHP_SELF'] . '</br>';
     <title>Document</title>
 </head>
 <body>
+
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="text" name="name">
+    <input type="submit" name="submit" value="submit">
+  </form>
     
 </body>
 </html>
