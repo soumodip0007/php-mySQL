c487a79c11773f9d18da0613fd749923cc7ed0bb
Soumodip Das <soumodipd567@gmail.com>
2022-12-12 11:02:54 +0530

    DN

---
+++ php&mySQL/chapter2.2/sandbox.php
@@ -0,0 +1,22 @@
+<?php
+
+//super globals
+
+//$_GET['name'], $_POST['name']
+
+echo $_SERVER['SERVER_NAME'] . '<br/>';
+echo $_SERVER['REQUEST_METHOD'] . '</br>';
+echo $_SERVER['SCRIPT_FILENAME'] . '</br>';
+echo $_SERVER['PHP_SELF'] . '</br>';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
